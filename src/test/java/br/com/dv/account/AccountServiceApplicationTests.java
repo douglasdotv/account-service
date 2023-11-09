c2e48c46e5b9cfaac7596722e3c90714fa0c2c88
Douglas Vieira <douglas16722@gmail.com>
2023-11-09 13:37:13 -0300

    Initial commit

---
+++ src/test/java/br/com/dv/account/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dv.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
