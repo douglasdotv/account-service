48f27e653dd404f525208284f3e827352508a353
Douglas Vieira <douglas16722@gmail.com>
2023-11-10 21:37:21 -0300

    refactor: fields
    Changed the order of modifiers from final static to static final across class fields to follow conventions. Added a newline after CustomUserDetailsService message field.

--- src/main/java/br/com/dv/account/exception/custom/PasswordLengthException.java
+++ src/main/java/br/com/dv/account/exception/custom/PasswordLengthException.java
@@ -2,7 +2,7 @@ package br.com.dv.account.exception.custom;
 
 public class PasswordLengthException extends RuntimeException {
 
-    private final static String EXCEPTION_MESSAGE = "The password must be at least 12 characters long.";
+    private static final String EXCEPTION_MESSAGE = "The password must be at least 12 characters long.";
 
     public PasswordLengthException() {
         super(EXCEPTION_MESSAGE);
