83fe25054cef6e10ebf27e111a4cdbba90a754ca
Douglas Vieira <douglas16722@gmail.com>
2023-11-11 22:26:06 -0300

    Add new payment JPQL query order by period desc
    Added a new custom query to PaymentRepository. Retrieves payments for a given employee
    email in a case-insensitive manner and orders them by period desc.

--- src/main/java/br/com/dv/account/repository/PaymentRepository.java
+++ src/main/java/br/com/dv/account/repository/PaymentRepository.java
@@ -2,12 +2,22 @@ package br.com.dv.account.repository;
 
 import br.com.dv.account.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     Optional<Payment> findByEmployeeEmailIgnoreCaseAndPeriod(String employeeEmail, String period);
+
+    @Query("""
+            SELECT e FROM Payment e
+            WHERE LOWER(e.employeeEmail) = LOWER(:employeeEmail)
+            ORDER BY e.periodYear DESC, e.periodMonth DESC
+            """)
+    List<Payment> findAllByEmployeeEmailOrderByPeriodDescIgnoreCase(@Param("employeeEmail") String employeeEmail);
 
     boolean existsByEmployeeEmailAndPeriod(String employeeEmail, String period);
 
