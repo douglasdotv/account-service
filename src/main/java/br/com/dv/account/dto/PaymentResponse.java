08a474dc54a5f5cafdc8102de139212435d76e4e
Douglas Vieira <douglas16722@gmail.com>
2023-11-11 22:22:26 -0300

    Update PaymentResponse DTO
    Changed PaymentResponse DTO fields due to new expected json format.

--- src/main/java/br/com/dv/account/dto/PaymentResponse.java
+++ src/main/java/br/com/dv/account/dto/PaymentResponse.java
@@ -3,11 +3,10 @@ package br.com.dv.account.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record PaymentResponse(
-        @JsonProperty("id")
-        Long userId,
         String name,
         @JsonProperty("lastname")
         String lastName,
-        String email
+        String period,
+        String salary
 ) {
 }
