06d49082cd0a144d975a5b93c84b351ee2341b3b
Douglas Vieira <douglas16722@gmail.com>
2023-11-11 22:11:50 -0300

    Add payment byEmailAndPeriod query methods
    Included queries to find and check if exists by e-mail (ignore case) and period in Payment repository.

--- src/main/java/br/com/dv/account/repository/PaymentRepository.java
+++ src/main/java/br/com/dv/account/repository/PaymentRepository.java
@@ -1,2 +1,14 @@
-package br.com.dv.account.repository;public interface PaymentRepository {
+package br.com.dv.account.repository;
+
+import br.com.dv.account.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByEmployeeEmailIgnoreCaseAndPeriod(String employeeEmail, String period);
+
+    boolean existsByEmployeeEmailAndPeriod(String employeeEmail, String period);
+
 }
