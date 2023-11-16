12770d24269f4eef54456879ae57e9f7899e50a1
Douglas Vieira <douglasdotv@hotmail.com>
2023-11-15 22:10:25 -0300

    Create Role entity and RoleRepository with findByName method

---
+++ src/main/java/br/com/dv/account/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package br.com.dv.account.repository;
+
+import br.com.dv.account.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+
+}
