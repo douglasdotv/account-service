c11f620b7aee9d7ba28a02d6e1579626b6990e27
Douglas Vieira <douglasdotv@hotmail.com>
2023-11-15 22:10:59 -0300

    Set JPA relationship between User and Role

--- src/main/java/br/com/dv/account/entity/User.java
+++ src/main/java/br/com/dv/account/entity/User.java
@@ -31,6 +31,14 @@ public class User {
     @OneToMany(mappedBy = "employee")
     private Set<Payment> payments = new HashSet<>();
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
     public void addPayment(Payment payment) {
         this.payments.add(payment);
         payment.setEmployee(this);
