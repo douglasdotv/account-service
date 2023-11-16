ffee8cac7c6a4ab65cd92729a184a01e2bb4001a
Douglas Vieira <douglas16722@gmail.com>
2023-11-15 22:48:42 -0300

    Add api/admin endpoints
    Added GET api/admin/user, DELETE api/admin/user/{user email} and PUT api/admin/user/role endpoints:
    - Implemented AdminController/AdminService.
    - Created RoleValidationService.
    - Added findAllByIdByOrderAsc query to UserRepository.
    - Created custom exceptions and updated GlobalExceptionHandler to handle errors.
    - Updated UserMapper with new mapping methods.

--- src/main/java/br/com/dv/account/repository/UserRepository.java
+++ src/main/java/br/com/dv/account/repository/UserRepository.java
@@ -3,9 +3,12 @@ package br.com.dv.account.repository;
 import br.com.dv.account.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findAllByOrderByIdAsc();
 
     Optional<User> findByEmailIgnoreCase(String email);
 
