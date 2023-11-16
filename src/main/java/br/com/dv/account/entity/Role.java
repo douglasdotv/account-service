4a93f1a0f44e653466689c3cdbd1e3732f0d200d
Douglas Vieira <douglasdotv@hotmail.com>
2023-11-15 22:10:25 -0300

    Create Role entity and RoleRepository with findByName method

---
+++ src/main/java/br/com/dv/account/entity/Role.java
@@ -0,0 +1,29 @@
+package br.com.dv.account.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter @Setter
+@Table(name = "role")
+public class Role {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    protected Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+}
