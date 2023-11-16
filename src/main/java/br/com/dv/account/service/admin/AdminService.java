f552fc906c86e3eb68e36313b7471632a09fd49b
Douglas Vieira <douglas16722@gmail.com>
2023-11-16 18:45:35 -0300

    refactor: improve user deletion and role update methods
    Renamed updateUserRole to updateUserRoles for clarity. Made deleteUser and updateUserRoles more readable by using helper methods.

--- src/main/java/br/com/dv/account/service/admin/AdminService.java
+++ src/main/java/br/com/dv/account/service/admin/AdminService.java
@@ -12,6 +12,6 @@ public interface AdminService {
 
     UserDeletionResponse deleteUser(String userEmail);
 
-    AdminUserResponse updateUserRole(RoleUpdateRequest roleUpdateRequest);
+    AdminUserResponse updateUserRoles(RoleUpdateRequest roleUpdateRequest);
 
 }
