a3143fbcb17dc42eb23dbaebcf41dcad765ed1e7
Douglas Vieira <douglas16722@gmail.com>
2023-11-19 17:27:23 -0300

    Create SecurityEvent entity

---
+++ src/main/java/br/com/dv/account/entity/SecurityEvent.java
@@ -0,0 +1,35 @@
+package br.com.dv.account.entity;
+
+import br.com.dv.account.enums.SecurityAction;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@Table(name = "security_event")
+public class SecurityEvent {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "security_event_id", nullable = false)
+    private Long id;
+
+    @Column(name = "date", nullable = false)
+    private LocalDateTime date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "action", nullable = false)
+    private SecurityAction action;
+
+    @Column(name = "subject", nullable = false)
+    private String subject;
+
+    @Column(name = "object", nullable = false)
+    private String object;
+
+    @Column(name = "path", nullable = false)
+    private String path;
+
+}
