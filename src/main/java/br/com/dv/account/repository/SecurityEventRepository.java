09d0d663c7a335331d93fa656ec3cd9aa9262a02
Douglas Vieira <douglas16722@gmail.com>
2023-11-19 17:34:40 -0300

    Add find all by id asc query to SecurityEventRepository

--- src/main/java/br/com/dv/account/repository/SecurityEventRepository.java
+++ src/main/java/br/com/dv/account/repository/SecurityEventRepository.java
@@ -3,5 +3,10 @@ package br.com.dv.account.repository;
 import br.com.dv.account.entity.SecurityEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SecurityEventRepository extends JpaRepository<SecurityEvent, Long> {
+
+    List<SecurityEvent> findAllByOrderByIdAsc();
+
 }
